022effe33297da1d1d4abb46b9a383b7f3610117
Arjen Visser <info@comvit.nl>
2012-02-24 15:04:47 +0100

    first commit with some LGE stuff

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 11 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#11 PREEMPT Fri Feb 24 11:44:42 CET 2012"
+#define LINUX_COMPILE_BY "arjen"
+#define LINUX_COMPILE_HOST "arjen-CQ56"
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
